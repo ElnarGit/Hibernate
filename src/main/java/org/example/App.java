33ee239cbbfab41ca93e8253b2035662a959ed6b
Elnar <elnar_saparov@mail.ru>
2023-01-07 18:21:43 +0600

    OneToMany

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -7,10 +7,6 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-
 public class App {
     public static void main(String[] args) {
         Configuration configuration = new Configuration().addAnnotatedClass(Person.class)
@@ -23,12 +19,7 @@ public class App {
 
             session.beginTransaction();
 
-            Person person = session.get(Person.class,2);
-            Item item = session.get(Item.class, 1);
 
-            item.setOwner(person);
-            person.getItems().add(item);
-            item.getOwner().getItems().remove(item);
 
 
            session.getTransaction().commit();
